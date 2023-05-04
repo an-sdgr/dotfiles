6e6349c5e230e294beccabfbaafc90f39e077f95
Austin Nason <austin.nason@schrodinger.com>
2023-05-04 12:52:24 -0700

    bootstrap

--- dot_local/bin/executable_bootstrap.sh
+++ dot_local/bin/executable_bootstrap.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 # nason bootstrap
-#
 
-echo "hi"
+# brew installer brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+brew install \
+  ansible-lint \
+  chezmoi \
+  cmake \
+  gh \
+  go \
+  ipcalc \
+  iperf3 \
+  jq \
+  liquidprompt \
+  neovim \
+  nmap \
+  packer \
+  shellcheck \
+  telnet \
+  terraform-docs \
+  tfenv \
+  the_silver_searcher \
+  tree \
+  visual-studio-code \
+  wget
+
+# TILING ONLY
+brew install \
+  koekeishiya/formulae/skhd \
+  koekeishiya/formulae/yabai
+
+# tfenv
+tfenv install latest
+tfenv use latest
+
+# vim directories
+mkdir -p ~/.vim/autoload
+mkdir ~/.vim/backup
+mkdir ~/.vim/bundle
+mkdir ~/.vim/cache
+mkdir ~/.vim/pack
+mkdir ~/.vim/plugged
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
+
 
